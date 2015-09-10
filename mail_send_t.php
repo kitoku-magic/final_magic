0cb55e9a68a65317016cdb6d99c50b5c5fa0a126
kitoku_magician <kitoku.magic@gmail.com>
2015-09-10 15:40:35 +0900

    CCとBCCの追加に対応

--- mail_send_t.php
+++ mail_send_t.php
@@ -11,19 +11,19 @@ $rows = explode("\n", $contents);
 // 1行ずつ内容を整理していく
 foreach ($rows as $row)
 {
-  $cols = explode(" ", $row);
-  if (7 <= count($cols))
-  {
-    // cols[6]の中にアクセスパスが入っている
-    if (true === array_key_exists($cols[6], $access_details))
-    {
-      $access_details[$cols[6]]++;
-    }
-    else
-    {
-      $access_details[$cols[6]] = 1;
-    }
-  }
+	$cols = explode(" ", $row);
+	if (7 <= count($cols))
+	{
+		// cols[6]の中にアクセスパスが入っている
+		if (true === array_key_exists($cols[6], $access_details))
+		{
+			$access_details[$cols[6]]++;
+		}
+		else
+		{
+			$access_details[$cols[6]] = 1;
+		}
+	}
 }
 
 arsort($access_details);
@@ -32,7 +32,7 @@ $body = '';
 // 本文の内容は「ファイルパス アクセス数」として1行ずつ表示させる
 foreach ($access_details as $file_path => $access_count)
 {
-  $body .= $file_path . ' ' . $access_count . "\n";
+	$body .= $file_path . ' ' . $access_count . "\n";
 }
 
 $ret = null;
@@ -46,10 +46,14 @@ $mail_obj->set_from('adihcu0@gmail.com');
 $mail_obj->set_name_from('送信元サーバー');
 $mail_obj->set_to('adihcu0@gmail.com');
 $mail_obj->set_name_to('自分');
+$mail_obj->set_cc('');
+$mail_obj->set_name_cc('');
+$mail_obj->set_bcc('');
+$mail_obj->set_name_bcc('');
 // 件名を「対象日 アクセス数」とする
 $mail_obj->set_subject(date('Y/m/d', strtotime($target_day)) . ' アクセス数');
 $mail_obj->set_body($body);
 $ret = $mail_obj->send_mail($library_path);
 
-var_dump($ret);
-var_dump($mail_obj->get_smtp_response_message());
+echo $ret . "\n";
+echo $mail_obj->get_smtp_response_message() . "\n";
